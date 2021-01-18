28cac4941fbee92c606118cfeacea00970d2325f
hackerjerry <zhangjingyu81@gmail.com>
2021-01-18 22:00:39 +0800

    Linux_hackerjerry_bashrc

---
+++ sample00/ballgown.R
@@ -0,0 +1,5 @@
+library(ballgown)
+path = "/home/hackerjerry/Downloads/github/demo/"
+path = "/media/hackerjerry/TDDOWNLOAD/results/"
+vignette("ballgown")
+bg_demo = ballgown(dataDir=path, samplePattern='sample', meas = all)
