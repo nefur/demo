2c3c29aabbbf1773648f7f90935d1e6a930ab8f1
nefur <zhangjingyu81@gmail.com>
2021-05-10 11:05:13 +0800

    Delete ballgown.R

--- sample00/ballgown.R
+++
@@ -1,5 +0,0 @@
-library(ballgown)
-path = "/home/hackerjerry/Downloads/github/demo/"
-path = "/media/hackerjerry/TDDOWNLOAD/results/"
-vignette("ballgown")
-bg_demo = ballgown(dataDir=path, samplePattern='sample', meas = all)
